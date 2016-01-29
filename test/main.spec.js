c78003e95b5e724e988e9072fd66d42c66861055
just-boris <just-boris@yandex-team.ru>
2016-01-29 19:37:09 +0300

    use proper getters

--- test/main.spec.js
+++ test/main.spec.js
@@ -16,14 +16,14 @@ describe('Organization page', function(){
     it('open next page', function() {
         browser.click('.next_page');
         browser.waitForExist('.repo-list-item');
-        expect(browser.url().value).toBe('https://github.com/webdriverio?page=2');
+        expect(browser.getUrl()).toBe('https://github.com/webdriverio?page=2');
         expect(browser.elements('.repo-list-item').value).toHaveGreaterLength(2);
     });
 
     it('open project page', function() {
         browser.click('.repo-list-item [href="/webdriverio/webdriverio"]');
         browser.waitForExist('.repository-content');
-        expect(browser.url().value).toBe('https://github.com/webdriverio/webdriverio');
+        expect(browser.getUrl()).toBe('https://github.com/webdriverio/webdriverio');
         expect(browser.getText('.files .content').slice(1, 6)).toEqual([
             'bin',
             'docs',
