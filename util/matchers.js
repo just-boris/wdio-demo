4f19d958e25b71687260452bb2af1f71f1915bfd
Boris Serdiuk <just-boris@hotmail.com>
2019-12-04 02:09:22 +0100

    upgrade to v5

--- util/matchers.js
+++ util/matchers.js
@@ -1,18 +1,18 @@
 const expect = require('expect');
 
 function assertArray(actual) {
-    expect(actual && actual.length).toExist('Expected to have a value with `length` property');
+  expect(actual && typeof actual.length === 'number').toExist('Expected to have a value with `length` property');
 }
 
 expect.extend({
   toHaveLength(length, message) {
-      assertArray(this.actual);
-      expect(this.actual.length).toEqual(length, message);
+    assertArray(this.actual);
+    expect(this.actual.length).toEqual(length, message);
   },
 
   toHaveGreaterLength(length, message) {
-      assertArray(this.actual);
-      expect(this.actual.length).toBeGreaterThan(length, message);
+    assertArray(this.actual);
+    expect(this.actual.length).toBeGreaterThan(length, message);
   }
 });
 
