4582ca411135ea36b524a95f82e26b77476f5111
Boris Serdiuk <just-boris@hotmail.com>
2020-12-06 23:16:55 +0100

    migrate to webdriverio v6 and use async/await

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,32 +1,50 @@
 const expect = require('../util/matchers');
 
-describe('Organization page', function() {
-  beforeEach(function() {
+async function waitForSelector(selector) {
+  await (await browser.$(selector)).waitForExist();
+}
+
+async function clickSelector(selector) {
+  await (await browser.$(selector)).click();
+}
+
+async function dismissCookieBanner() {
+  const acceptButton = await browser.$('.js-cookie-consent-accept');
+  await acceptButton.click();
+  await acceptButton.waitForDisplayed({ reverse: true });
+}
+
+describe('Organization page', function () {
+  beforeEach(async () => {
     // force page reload
-    browser.url('about:blank');
-    browser.url('');
-    browser.$('.HeaderMenu').waitForExist();
+    await browser.url('about:blank');
+    await browser.url('');
+    await waitForSelector('.HeaderMenu');
   });
 
-  it('list repositories', function() {
-    expect(browser.$$('.repo-list .public')).toHaveLength(13);
+  it('list repositories', async () => {
+    const elements = await browser.$$('.repo-list .public');
+    expect(elements).toHaveLength(15);
   });
 
-  it('open project page', function() {
-    browser.$('.repo-list .public [href="/webdriverio/webdriverio"]').click();
-    browser.$('.repository-content').waitForExist();
-    expect(browser.getUrl()).toBe('https://github.com/webdriverio/webdriverio');
-    expect(
-      browser
-        .$$('.files .content')
-        .slice(1, 6)
-        .map(element => element.getText())
-    ).toEqual(['.github', 'docs', 'e2e', 'examples', 'packages']);
+  it('open project page', async () => {
+    await dismissCookieBanner();
+    await clickSelector('.repo-list .public [href="/webdriverio/webdriverio"]');
+    await waitForSelector('.repository-content');
+    expect(await browser.getUrl()).toBe('https://github.com/webdriverio/webdriverio');
+    const entries = await browser.$$('a[href*="/webdriverio/webdriverio/tree"]');
+    expect(await Promise.all(entries.slice(1, 6).map((element) => element.getText()))).toEqual([
+      '.github',
+      'docs',
+      'e2e',
+      'examples',
+      'packages',
+    ]);
   });
 
-  afterEach(function() {
+  afterEach(async function () {
     if (this.currentTest.state !== 'passed') {
-      browser.takeScreenshot();
+      await browser.takeScreenshot();
     }
   });
 });
